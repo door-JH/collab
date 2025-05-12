fa5d535e49ab2df42fd37dccffe68bf3bd8abb54
elogio.mi@gmail.com <elogio.mi@gmail.com>
2025-05-12 14:52:36 +0900

    [FEAT] 공통 소스 추가

---
+++ src/main/java/app/api/controller/EmpAPIController.java
@@ -0,0 +1,14 @@
+package app.api.controller;
+
+import app.repository.EmpRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class EmpAPIController {
+
+    private final EmpRepository empRepository;
+
+
+}
